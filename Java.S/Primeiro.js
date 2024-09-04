acead7aa9cf9d3157c507cc2d79f479bb18b77d6
Joao Pedro <73714674+Johny07159@users.noreply.github.com>
2024-09-04 08:39:27 -0300

    Delete Java.S directory

--- Java.S/Primeiro.js
+++
@@ -1,3 +0,0 @@
-function mostraMensagem() {
-    return window.confirm('Você tem certeza que deseja adicionar este item ao carrinho?');
-}
--- Java.S/ativ36.js
+++
@@ -1,5 +0,0 @@
-  
-        let nome = window.prompt("Qual seu nome");
-        
-        window.alert(`Ola,${nome}!Seja bem-vindo!`)
-        window.alert('javascript enviado com sucesso!.');
